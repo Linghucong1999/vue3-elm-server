b75f7ba8b514d443df2bf598962ed8540a3d9b70
mika <2865911620@qq.com>
2023-06-13 23:47:37 +0800

    数据文件

---
+++ InitData/activity.js
@@ -0,0 +1,10 @@
+module.exports= [
+    {
+        description:'',
+        icon_color:'3fbde6',
+        icon_name:'品',
+        id:8,
+        name:'品牌商家',
+        rangking_weight:7
+    }
+]
---
+++ InitData/delivery.js
@@ -0,0 +1,6 @@
+module.exports = {
+    color: '57a9ff',
+    id: 1,
+    is_solid: true,
+    text: '顺丰快运'
+}
