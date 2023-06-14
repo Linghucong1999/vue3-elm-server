c260af35494bf70bb109bc7428b46395ad62e4f0
mika <2865911620@qq.com>
2023-06-14 17:26:02 +0800

    更新通过分类id获取数据

--- controller/shopping/category.js
+++ controller/shopping/category.js
@@ -11,26 +11,42 @@ class Category extends BaseComponent {
     //获取所有餐馆分类和数量
     async getCategories(req, res, next) {
         try {
-            const categories= await CategoryModel.find({},'-_id');
+            const categories = await CategoryModel.find({}, '-_id');
             res.send(categories);
         } catch (err) {
             console.log('获取categories失败');
             res.send({
-                status:0,
-                type:'ERROR_DATA',
-                message:'获取categories失败'
+                status: 0,
+                type: 'ERROR_DATA',
+                message: '获取categories失败'
             })
         }
     }
 
-    async addCategory(type){
-        try{
+    async addCategory(type) {
+        try {
             await CategoryModel.addCategory(type);
-        }catch(err){
+        } catch (err) {
             console.log('增加catergory数量失败');
+        }
+    }
+
+    async findById(id) {
+        try {
+            const CateEntity = await CategoryModel.findOne({ 'sub_categories.id': id });
+            let categoName = CateEntity.name;
+            CateEntity.sub_categories.forEach(item => {
+                if (item.id == id) {
+                    categoName += '/' + item.name;
+                }
+            })
+            return categoName;
+        } catch (err) {
+            console.log("通过category id获取数据失败");
+            throw new Error(err);
         }
     }
 
 }
 
-module.exports=new Category();
+module.exports = new Category();
