d421229e1eaacfefe89e2b4118f2b7a95c645772
mika <2865911620@qq.com>
2023-06-24 21:44:07 +0800

    新增查询api使用次数路由

--- routers/index.js
+++ routers/index.js
@@ -1,6 +1,8 @@
 const shopping = require('./shopping.js');
 const admin=require('./admin.js');
+const statis=require('./statis.js');
 module.exports = app => {
     app.use('/shopping', shopping);
     app.use('/admin',admin);
+    app.use('/statis',statis);
 }
---
+++ routers/statis.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const Statis = require('../controller/statis/statis.js');
+
+const router = express.Router();
+
+router.get('/api/:data/count', Statis.apiCount);
+router.get('/api/count',Statis.apiAllcount);
+router.get('/api/all',Statis.allApiRecord);
+router.get('/user/:data/count',Statis.userCount);
+
+module.exports = router;
