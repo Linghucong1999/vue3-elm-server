d7b4fea00768ca305e0397c78c2feeaa236bc62d
mika <2865911620@qq.com>
2023-07-07 16:54:29 +0800

    添加一些测试数据

---
+++ InitData/entry.js
@@ -0,0 +1,21 @@
+module.exports = [
+    {
+        id: 1,
+        is_in_serving: true,
+        description: '附近美食一网打尽',
+        title: '美食',
+        link: '',
+        img_url: '',
+        icon_url: '',
+        title_color: ''
+    },{
+        id:2,
+        is_in_serving: true,
+        description: '苦了累了,来点甜的',
+        title: '甜品',
+        link: '',
+        img_url: '',
+        icon_url: '',
+        title_color: ''
+    }
+]
