b687ca5d5199b76249e2c5b30930020bfe4f36be
mika <2865911620@qq.com>
2023-07-21 15:39:16 +0800

    新增订单管理员API的调用

--- controller/statis/statis.js
+++ controller/statis/statis.js
@@ -3,14 +3,15 @@ const UserInfoModel = require('../../models/v2/userinfo.js');
 const OrderModel = require('../../models/bos/order.js');
 const AdminModel = require('../../models/admin/admin.js');
 
+//相关api请求次数
 class Statis {
     constructor() {
 
     }
 
     async apiCount(req, res, next) {
-        const data = req.params.data;
-        if (!data) {
+        const date = req.params.data;
+        if (!date) {
             res.send({
                 status: 0,
                 type: 'ERROR_PARAMS',
@@ -20,10 +21,12 @@ class Statis {
         }
 
         try {
-            const count = await StatisModel.find({ data }).count();
+            const count = await StatisModel.find({ date }).count();
             res.send({
                 status: 1,
-                count
+                count,
+                message: date
+
             })
         } catch (err) {
             res.send({
@@ -75,12 +78,74 @@ class Statis {
             return
         }
 
-        try{
-            const count=await UserInfoModel.find({registe_time:eval()})
-        }catch(err){
-
+        try {
+            const count = await UserInfoModel.find({ registe_time: eval('/^' + data + '/gi') }).count();
+            res.send({
+                status: 1,
+                count
+            })
+        } catch (err) {
+            res.send({
+                status: 0,
+                type: 'ERROR_GET_USER_REGISTE_COUNT',
+                message: '获取当天注册的人数失败'
+            })
         }
     }
+
+    async adminCount(req, res, next) {
+        const data = req.params.data;
+        if (!data) {
+            res.send({
+                status: 0,
+                type: 'ERROR_PARAMS',
+                message: '参数错误'
+            })
+            return
+        }
+
+        try {
+            const count = await AdminModel.find({ creat_time: eval('/^' + data + '/gi') }).count();
+            res.send({
+                status: 1,
+                count,
+            })
+        } catch (err) {
+            res.send({
+                status: 0,
+                type: 'ERROR_GET_ADMIN_REGISTE_COUNT',
+                message: '获取当天注册管理员人数失败'
+            })
+        }
+    }
+
+    async orderCount(req, res, next) {
+        const data = req.params.data;
+        if (!data) {
+            res.send({
+                status: 0,
+                type: 'ERROR_PARAMS',
+                message: '参数错误'
+            })
+            return
+        }
+
+        try {
+            const count = await OrderModel.find({ formatted_created_at: eval('/^' + data + '/gi') }).count()
+            res.send({
+                status: 1,
+                count
+            })
+        } catch (err) {
+            res.send({
+                status: 0,
+                type: 'ERROR_GET_ORDER_COUNT',
+                message: '获取当天订单数量失败'
+            })
+        }
+    }
+
+
 }
 
 module.exports = new Statis();
