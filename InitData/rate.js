866b2d722f5c5b4ee789a5bcdb46b2a7cc982cf0
mika <2865911620@qq.com>
2023-07-26 22:36:18 +0800

    评论评分数据

---
+++ InitData/rate.js
@@ -0,0 +1,98 @@
+const ratingList = [
+    {
+        "avatar": "",
+        "highlights": [],
+        "item_ratings": [
+            {
+                "food_id": 1,
+                "fodd_name": "麦乐堡",
+                "image_hash": "",
+                "is_valid": 1
+            }, {
+                "food_id": 2,
+                "fodd_name": "风味炸鸡",
+                "image_hash": "",
+                "is_valid": 1
+            }
+        ],
+        "rated_at": "2023-01-12",
+        "rating_star": 5,
+        "rating_text": "",
+        "tags": [],
+        "time_spent_desc": "准时达",
+        "username": "4********b",
+    }, {
+        "avatar": "",
+        "highlights": [],
+        "item_ratings": [
+            {
+                "food_id": 508809480,
+                "food_name": "冰柠檬红茶（标准份）",
+                "image_hash": "",
+                "is_valid": 1
+            }
+        ],
+        "rated_at": "2017-01-18",
+        "rating_star": 5,
+        "rating_text": "",
+        "tags": [],
+        "time_spent_desc": "",
+        "username": "，******C"
+    }, {
+        "avatar": "",
+        "highlights": [],
+        "item_ratings": [],
+        "rated_at": "2017-02-10",
+        "rating_star": 5,
+        "rating_text": "",
+        "tags": [],
+        "time_spent_desc": "",
+        "username": "试******春"
+    }, {
+        "avatar": "",
+        "highlights": [],
+        "item_ratings": [],
+        "rated_at": "2017-02-09",
+        "rating_star": 5,
+        "rating_text": "",
+        "tags": [],
+        "time_spent_desc": "",
+        "username": "景***0"
+    }
+]
+
+const scores = {
+    "compare_rating": 0.76869,
+    "deliver_time": 400,
+    "food_score": 4.76378,
+    "order_rating_amount": 473,
+    "overall_score": 4.72836,
+    "service_score": 4.69295
+}
+
+const tags = [
+    {
+        "count": 473,
+        "name": "全部",
+        "unsatisfied": false
+    }, {
+        "count": 453,
+        "name": "满意",
+        "unsatisfied": false,
+    }, {
+        "count": 47,
+        "name": "味道好",
+        "unsatisfied": false
+    }, {
+        "count": 15,
+        "name": "包装精美",
+        "unsatisfied": false
+    },
+]
+
+
+module.exports = {
+    ratingList,
+    scores,
+    tags,
+}
