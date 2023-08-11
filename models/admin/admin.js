ce6086c66a8b96e22e80bb21962457501d58b747
mika <2865911620@qq.com>
2023-08-11 23:27:58 +0800

    管理员模型

---
+++ models/admin/admin.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const adminSchema = new Schema({
+    user_name: String,
+    password: String,
+    id: Number,
+    creat_time: String,
+    admin: { type: String, default: '管理员' },
+    status: Number,  //1:普通管理员,2:超级管理员
+    avatar: { type: String, default: 'default.jpg' },
+    city: String
+})
+
+adminSchema.index({ id: 1 });
+const Admin = mongoose.model('Admin', adminSchema);
+
+module.exports = Admin;
